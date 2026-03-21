8de233418388ecee5d260bc155a23e9d17eb8b84
niclesanti <niclesanti687@gmail.com>
2026-03-21 17:30:01 -0300

    spring initializr

---
+++ reservation-backend/src/main/java/ar/dev/niclesanti/reservation/ReservationBackendApplication.java
@@ -0,0 +1,13 @@
+package ar.dev.niclesanti.reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationBackendApplication.class, args);
+	}
+
+}
---
+++ reservation-backend/src/test/java/ar/dev/niclesanti/reservation/ReservationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.dev.niclesanti.reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
